41060d48a60b5496be26ebe6f9b321744d8fe585
anastasiakurova <kyrova.nastia@gmail.com>
2023-10-13 16:28:27 +0400

    add social animations with native tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { MainPage } from '@/components/MainPage/MainPage';
 import { MainSection } from '@/components/MainSection/MainSection';
 import { Metadata } from 'next';
 import AboutSection from '@/components/AboutSection/AboutSection';
+import SubscriptionsSection from '@/components/SubscriptionsSection/SubscriptionsSection';
 
 export const metadata: Metadata = {
   title: 'Animation page',
@@ -14,6 +15,7 @@ export default function Home() {
     <MainPage>
       <MainSection />
       <AboutSection />
+      <SubscriptionsSection />
     </MainPage>
   );
 }
---
+++ src/components/MagneticWrapper/MagneticWrapper.tsx
@@ -0,0 +1,55 @@
+import React, { RefObject, useEffect, useRef, useState } from 'react';
+import { ReactChildrenType } from '@/types/commonTypes';
+
+const MagneticGsapWrapper = ({ children }: ReactChildrenType) => {
+  const magneticRef: RefObject<HTMLInputElement> = useRef(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const prevPositionRef = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const onMouseMove = (e: MouseEvent) => {
+      if (magneticRef.current !== null) {
+        const { clientX, clientY } = e;
+        const { height, width, left, top } =
+          magneticRef.current.getBoundingClientRect();
+        const x = clientX - (left + width / 2);
+        const y = clientY - (top + height / 2);
+
+        const deltaX = x - prevPositionRef.current.x;
+        const deltaY = y - prevPositionRef.current.y;
+
+        setPosition((prevPosition) => ({
+          x: prevPosition.x + deltaX * 0.5, // freeze
+          y: prevPosition.y + deltaY * 0.5,
+        }));
+        prevPositionRef.current = { x, y };
+      }
+    };
+    const onMouseLeave = () => {
+      setPosition({ x: 0, y: 0 });
+      prevPositionRef.current = { x: 0, y: 0 };
+    };
+
+    magneticRef.current?.addEventListener('mousemove', onMouseMove);
+    magneticRef.current?.addEventListener('mouseleave', onMouseLeave);
+
+    return () => {
+      magneticRef.current?.removeEventListener('mousemove', onMouseMove);
+      magneticRef.current?.removeEventListener('mouseleave', onMouseLeave);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={magneticRef}
+      className={'icon'}
+      style={{
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MagneticGsapWrapper;
